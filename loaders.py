cfe437b80155d5be09475e322bf5abc83e4aa91a
klean2050 <k.avramidis@windowslive.com>
2021-09-25 23:32:52 +0300

    bug fix at crs_loader

--- loaders.py
+++ loaders.py
@@ -64,6 +64,8 @@ def get_de_loader(path, dur, subject, num_tracks=34):
 def get_crs_loader(path, dur, subject):
 
         c = '0'+str(subject+1) if subject<9 else str(subject+1)
+        segs = 60 - dur + 1
+
         eeg_samples = np.load(path+'{}sec_de/P{}_feats.npy'.format(dur,c)).reshape(34,58,32,-1)
         eeg_labels  = np.load(path+'{}sec_de/P{}_annot.npy'.format(dur,c)).reshape(34,58,-1)
         mus_samples = np.load(path+'stimuli/tracks_embeds.npy')
@@ -78,10 +80,10 @@ def get_crs_loader(path, dur, subject):
                 X_train = eeg_samples[train_index]; X_test = eeg_samples[test_index]
                 y_train = eeg_labels[train_index];  y_test = eeg_labels[test_index]
 
-                eeg['train{}'.format(fold)] = X_train.reshape(X_train.shape[0]*58,32,4*dur)
-                eeg['test{}'.format(fold)]  = X_test.reshape(X_test.shape[0]*58,32,4*dur)
-                lab['train{}'.format(fold)] = y_train.reshape(y_train.shape[0]*58,-1)
-                lab['test{}'.format(fold)]  = y_test.reshape(y_test.shape[0]*58,-1)
+                eeg['train{}'.format(fold)] = X_train.reshape(X_train.shape[0]*segs,32,4*dur)
+                eeg['test{}'.format(fold)]  = X_test.reshape(X_test.shape[0]*segs,32,4*dur)
+                lab['train{}'.format(fold)] = y_train.reshape(y_train.shape[0]*segs,-1)
+                lab['test{}'.format(fold)]  = y_test.reshape(y_test.shape[0]*segs,-1)
 
         for x in eeg.keys(): mus[x], mlab[x] = make_pairs(eeg[x], lab[x], mus_samples, mus_labels, dur)
         return eeg, mus, lab, mlab
@@ -96,6 +98,6 @@ def get_loader(path, mode, batch_size, dur, subject='0'):
                 eeg, mus, elabels, mlabels = get_crs_loader(path, dur, subject)
                 dataset = {x:CROSSDataSet(eeg=eeg[x], mus=mus[x], elabels=elabels[x], mlabels=mlabels[x]) for x in eeg.keys()}
         
-        dataloader = {x: DataLoader(dataset[x], batch_size=batch_size, num_workers=0, drop_last=True) for x in dataset.keys()}
+        dataloader = {x: DataLoader(dataset[x], batch_size=batch_size, num_workers=0, drop_last=True, shuffle=True) for x in dataset.keys()}
         return dataloader
 
