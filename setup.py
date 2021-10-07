0c30c42c3cebd664599f583ba4b83e72cd78a86c
klean2050 <k.avramidis@windowslive.com>
2021-10-07 12:10:35 +0300

    modified setup

--- setup.py
+++ setup.py
@@ -20,4 +20,13 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+    install_requires=[
+        "matplotlib>=3.4.3",
+        "musicnn>=0.1.0",
+        "numpy>=1.21.2",
+        "scikit_learn>=1.0",
+        "scipy>=1.7.1",
+        "torch>=1.9.1",
+        "tqdm>=4.62.3"
+    ]
 )
